41151d9ab5171aaa11057097d0931de39c0f0505
DmytroYesyp <91474410+DmytroYesyp@users.noreply.github.com>
2022-04-11 15:21:12 +0300

    Add files via upload

---
+++ src/main/java/com/sportshub/SportsHubApplication.java
@@ -0,0 +1,13 @@
+package com.sportshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportshub/SportsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
