84262da36998fd3a133c2b0e1b1a5ae250ee69a6
DmytroYesyp <91474410+DmytroYesyp@users.noreply.github.com>
2022-04-30 17:23:22 +0300

    Update SportsHubApplication.java

--- src/main/java/com/sportshub/SportsHubApplication.java
+++ src/main/java/com/sportshub/SportsHubApplication.java
@@ -2,6 +2,9 @@ package com.sportshub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SportsHubApplication {
