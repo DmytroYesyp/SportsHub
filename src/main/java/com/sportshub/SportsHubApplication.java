bb53ffe22cbc460ae8c74ba84004a897c311b4ca
Dmytro Yesyp <dmytro.yesyp.knm.2020@lpnu.ua>
2022-04-30 17:20:22 +0300

    Repairing PEncoder and more cleaning

--- src/main/java/com/sportshub/SportsHubApplication.java
+++ src/main/java/com/sportshub/SportsHubApplication.java
@@ -2,11 +2,18 @@ package com.sportshub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SportsHubApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SportsHubApplication.class, args);
+    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
