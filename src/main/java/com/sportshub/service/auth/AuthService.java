227a8b536ad6da694cf34df11afc7e804db488b0
DESKTOP-EU9GOJU\Acer <Julia.Zanevych@gmail.com>
2022-04-30 16:06:19 +0300

    REmove

--- src/main/java/com/sportshub/service/auth/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.sportshub.service.auth;
-
-import com.sportshub.dto.auth.token.AuthTokenDto;
-import com.sportshub.dto.user.credentials.UserCredentials;
-
-public interface AuthService {
-    AuthTokenDto login(UserCredentials userCredentials);
-    AuthTokenDto getRefreshedToken();
-}
--- src/main/java/com/sportshub/service/jwt/JwtService.java
+++
@@ -1,7 +0,0 @@
-package com.sportshub.service.jwt;
-
-import com.sportshub.dto.user.data.UserData;
-
-public interface JwtService {
-    String generateToken(UserData userData);
-}
