3dff0a98e5ddedb567cf9eb9465983ae5e7ef732
Dmytro Yesyp <dmytro.yesyp.knm.2020@lpnu.ua>
2022-05-05 22:13:57 +0300

    header profile (log-in)

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,11 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {mainPage} from "../services/main-page.service";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+
+
+  email: string;
+
+  firstName: any;
+  lastName: any;
+
+  user: Object;
+
+  constructor(private mainpage: mainPage) {
+  }
+  getUserFromToken(){
+
+    const token = localStorage.getItem('auth-token');
+    if (!token)
+      return console.log("error");
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    const tmp = JSON.parse(jsonPayload);
+
+
+    this.mainpage.getUserByEmail(tmp.sub).subscribe(data => {
+      this.user = data
+
+      for (let [key, value] of Object.entries(this.user)) {
+        if (key == "firstName")
+          this.firstName = value;
+        if (key == "lastName")
+          this.lastName = value
+      }
+
+    });
+    //console.log(this.firstName , this.lastName);
+    return tmp;
+  }
+
+
   search: String = "";
   foods: any;
+
+
+  ngOnInit(): void {
+
+
+    this.mainpage.getUserByEmail(this.getUserFromToken())
+
+
+    // this.firstName = user.firstName;
+    // this.lastName = user.lastName;
+  }
 }
