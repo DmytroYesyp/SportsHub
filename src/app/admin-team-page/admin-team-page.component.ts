1d4f44f7c1f471a67ec397dc0109db2f3256d8bf
Dmytro Yesyp <dmytro.yesyp.knm.2020@lpnu.ua>
2022-05-12 04:09:43 +0300

    image cloud processing

---
+++ src/app/admin-team-page/admin-team-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-team-page',
+  templateUrl: './admin-team-page.component.html',
+  styleUrls: ['./admin-team-page.component.css']
+})
+export class AdminTeamPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
