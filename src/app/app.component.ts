46a289f4ee660176d5d3903d7cda54f77861cc70
Yaroslav-Rohan <55649508+Yaroslav-Rohan@users.noreply.github.com>
2022-05-19 17:24:21 +0300

    "end with admin"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,15 +24,20 @@ export class AppComponent implements OnInit{
   getUserFromToken(){
     const token = localStorage.getItem('auth-token');
     if (!token)
-      return console.log("error");
+      return console.log("No such token");
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
     const tmp = JSON.parse(jsonPayload);
-    console.log(tmp.roles[0])
     return tmp.roles[0];
+  }
+
+  isAdmin(){
+    let role = this.getUserFromToken()
+    console.log(role)
+    return role == "admin";
   }
 
   ngOnInit() {
