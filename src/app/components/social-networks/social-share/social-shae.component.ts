ab651bbf470c12bd3bcbc74049e808ca8aa159b1
Dmytro Yesyp <dmytro.yesyp.knm.2020@lpnu.ua>
2022-06-02 04:46:30 +0300

    initial functionality of social components

--- src/app/components/social-networks/social-share/social-shae.component.ts
+++ src/app/components/social-networks/social-share/social-shae.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Team} from "../../../admin-team-page/admin-team-page.component";
+import {HttpClient} from "@angular/common/http";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-social-share',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social-shae.component.css']
 })
 export class SocialShaeComponent implements OnInit {
+  form: FormGroup
+  show: Boolean = false
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+
+  reverse(): void {
+    this.show = !this.show
+  }
+
+  list: socialFollow[];
 
   ngOnInit(): void {
+    this.http.get<socialFollow[]>(`http://localhost:8080/api/socialShare/getAllShares`).subscribe(data => {
+      this.list = data
+    })
+    this.form = new FormGroup({
+      url: new FormControl('', [Validators.required]),
+      pictogram: new FormControl('', [Validators.required]),
+    })
+
   }
+
+}
+
+export class socialFollow {
+  url: string
+  pictogram: string
 
 }
--- src/app/services/team.ts
+++
@@ -1,6 +0,0 @@
-// export interface Team{
-//   name: string
-//   image_url: string
-//   coach: string
-//   state: string
-// }
