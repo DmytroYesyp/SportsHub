9afc73cd6fca5d37a4ab99787250a9f1a4ecf7a7
shliakhovskyids <dmytro.shliakhovskyi.knm.2020@lpnu.ua>
2022-07-25 20:05:15 +0300

    added pages about/contact-us/admin-footer

--- src/app/admin-footer/admin-footer.component.ts
+++ src/app/admin-footer/admin-footer.component.ts
@@ -5,6 +5,7 @@ import {AppComponent} from "../app.component";
 import {TranslateService} from "@ngx-translate/core";
 import {FormGroup, FormControl, Validators} from "@angular/forms";
 import {HttpClient, HttpParams} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 
 @Component({
@@ -87,7 +88,7 @@ export class AdminFooterComponent implements OnInit {
       this.isAdmin = true;
     }
 
-    this.http.get('http://localhost:8080/datelimits')
+    this.http.get(environment.URL + `datelimits`)
       .subscribe((Response) => {
         this.dateLimitList = <Array<any>>Response
         if (this.dateLimitList[0]['datelim']>this.dateLimitList[1]['datelim']) {
@@ -101,57 +102,57 @@ export class AdminFooterComponent implements OnInit {
         this.booList[1] = this.dateLimitList[1]['datelim'] != 0;
       });
 
-    this.http.get('http://localhost:8080/page-content')
+    this.http.get(environment.URL + `datelimits`)
       .subscribe(Response => {
         this.list=(<Array<any>>Response);
         console.log(this.list)
       })
 
-    this.http.get('http://localhost:8080/datelimits/4')
+    this.http.get(environment.URL + `datelimits/4`)
       .subscribe((Response) => {
         this.newsletterPublished[0] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/5')
+    this.http.get(environment.URL + `datelimits/5`)
       .subscribe((Response) => {
         this.newsletterPublished[1] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/6')
+    this.http.get(environment.URL + `datelimits/6`)
       .subscribe((Response) => {
         this.companyInfoPublished[0] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/7')
+    this.http.get(environment.URL + `datelimits/7`)
       .subscribe((Response) => {
         this.companyInfoPublished[1] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/8')
+    this.http.get(environment.URL + `datelimits/8`)
       .subscribe((Response) => {
         this.companyInfoPublished[2] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/9')
+    this.http.get(environment.URL + `datelimits/9`)
       .subscribe((Response) => {
         this.companyInfoPublished[3] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/10')
+    this.http.get(environment.URL + `datelimits/10`)
       .subscribe((Response) => {
         this.companyInfoPublished[4] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/11')
+    this.http.get(environment.URL + `datelimits/11`)
       .subscribe((Response) => {
         this.companyInfoPublished[5] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/12')
+    this.http.get(environment.URL + `datelimits/12`)
       .subscribe((Response) => {
         this.contributorsPublished[0] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/13')
+    this.http.get(environment.URL + `datelimits/13`)
       .subscribe((Response) => {
         this.contributorsPublished[1] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/14')
+    this.http.get(environment.URL + `datelimits/14`)
       .subscribe((Response) => {
         this.contributorsPublished[2] = Response['datelim'] != 0
       })
-    this.http.get('http://localhost:8080/datelimits/15')
+    this.http.get(environment.URL + `datelimits/15`)
       .subscribe((Response) => {
         this.contributorsPublished[3] = Response['datelim'] != 0
       })
@@ -184,13 +185,13 @@ export class AdminFooterComponent implements OnInit {
 
   newsletterPublish(id, i){
     if (this.newsletterPublished[i]){
-      this.http.put('http://localhost:8080/datelimits/' + id, {"datelim": 0})
+      this.http.put(environment.URL + `datelimits` + id, {"datelim": 0})
         .subscribe(() => {
           this.newsletterPublished[i] = false
         });
     }
     else{
-      this.http.put('http://localhost:8080/datelimits/' + id, {"datelim": 1})
+      this.http.put(environment.URL + `datelimits` + id, {"datelim": 1})
         .subscribe(() => {
           this.newsletterPublished[i] = true
         });
@@ -199,13 +200,13 @@ export class AdminFooterComponent implements OnInit {
 
   companyInfoPublish(id, i){
     if (this.companyInfoPublished[i]){
-      this.http.put('http://localhost:8080/datelimits/' + id, {"datelim": 0})
+      this.http.put(environment.URL + `datelimits` + id, {"datelim": 0})
         .subscribe(() => {
           this.companyInfoPublished[i] = false
         });
     }
     else{
-      this.http.put('http://localhost:8080/datelimits/' + id, {"datelim": 1})
+      this.http.put(environment.URL + `datelimits` + id, {"datelim": 1})
         .subscribe(() => {
           this.companyInfoPublished[i] = true
         });
@@ -214,13 +215,13 @@ export class AdminFooterComponent implements OnInit {
 
   contributorsPublish(id, i){
     if (this.contributorsPublished[i]){
-      this.http.put('http://localhost:8080/datelimits/' + id, {"datelim": 0})
+      this.http.put(environment.URL + `datelimits` + id, {"datelim": 0})
         .subscribe(() => {
           this.contributorsPublished[i] = false
         });
     }
     else{
-      this.http.put('http://localhost:8080/datelimits/' + id, {"datelim": 1})
+      this.http.put(environment.URL + `datelimits` + id, {"datelim": 1})
         .subscribe(() => {
           this.contributorsPublished[i] = true
         });
